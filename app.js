25f62a7c4ade8fe2d98452bae70ee5e8483f8b7f
fedeperin <federicoperin@outlook.com.ar>
2021-08-22 12:34:30 -0300

    Change some ifs by ternary operators and functions by arrow functions

--- app.js
+++ app.js
@@ -10,42 +10,17 @@ let star = `<svg viewBox="0 0 16 16"><path d="M8 .25a.75.75 0 01.673.418l1.882 3
 
 let fork = `<svg viewBox="0 0 16 16"><path d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>`
 
-githubRepoCards.forEach(githubRepoCard => {
-    fetchCards(githubRepoCard.dataset.user, githubRepoCard)
+const newCard = (title, htmlUrl, desc, githubRepoCard, language, stars, forks) => {
+    targetBlank = githubRepoCard.dataset.targetBlank ? "target='_blank' rel='noopener'" : ""
 
-    if(githubRepoCard.dataset.background) {
-        githubRepoCard.style.background = githubRepoCard.dataset.background
-    }
-})
+    desc = desc ? desc : ''
 
-function newCard(title, htmlUrl, desc, githubRepoCard, language, stars, forks) {
-    if(githubRepoCard.dataset.targetBlank == true) {
-        targetBlank = 'target="_blank" rel="noopener"'
-    }else if(githubRepoCard.dataset.targetBlank == false) {
-        targetBlank = ''
-    }
+    language = language ? `<p class="lang"><span class="lang-color ${ language }"></span> ${ language }</p>` : ''
 
-    if(desc == null) {
-        desc = ''
-    }
+    showStars = stars <= 0 ? '' : `<a class="stars stars-forks" href="${ htmlUrl + '/stargazers' }" ${ targetBlank }>${ star } <span>${ stars }</span></a>`
 
-    if (language !== null) {
-        language = `<p class="lang"><span class="lang-color ${ language }"></span> ${ language }</p>`
-    }else {
-        language = ''
-    }
-
-    if(stars !== 0) {
-        showStars = `<a class="stars stars-forks" href="${ htmlUrl + '/stargazers' }" ${ targetBlank }>${ star } <span>${ stars }</span></a>`
-    }else {
-        showStars = ''
-    }
-
-    if(forks !== 0) {
-        showForks = `<a class="forks stars-forks" href="${ htmlUrl + '/network/members' }" ${ targetBlank }>${ fork } <span>${ forks }</span></a>`
-    }else {
-        showForks = ''
-    }
+    showForks = forks <= 0 ? '' : `<a class="forks stars-forks" href="${ htmlUrl + '/network/members' }" ${ targetBlank }>${ fork } <span>${ forks }</span></a>`
+    
     return `
     <div class="github-repo-card">
         <div>
@@ -63,7 +38,7 @@ function newCard(title, htmlUrl, desc, githubRepoCard, language, stars, forks) {
     `
 }
 
-function fetchCards(user, githubRepoCard) {
+const fetchCards = (user, githubRepoCard) => {
     
     fetch(`https://api.github.com/users/${ user }/repos`)
     .then(res => res.json())
@@ -95,3 +70,11 @@ function fetchCards(user, githubRepoCard) {
             })
         })
 }
+
+githubRepoCards.forEach(githubRepoCard => {
+    fetchCards(githubRepoCard.dataset.user, githubRepoCard)
+
+    if(githubRepoCard.dataset.background) {
+        githubRepoCard.style.background = githubRepoCard.dataset.background
+    }
+})
