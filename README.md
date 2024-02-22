f35994897afeb941d84421ddbdb6414f5c0f87d8
fedeperin <federicoperin2009@gmail.com>
2024-02-22 03:10:03 -0300

    Convert the entire element into a custom element

--- README.md
+++ README.md
@@ -1,37 +1,31 @@
 # Github Repo Cards
+> [!IMPORTANT]  
+> Github Repo Cards is now on it's version 2, it had many breaking changes since version 1. If you want to continue using the old version, please check the [1.0.10 version docs](https://www.npmjs.com/package/github-repo-cards/v/1.0.10)  
 
-GitHub Repo Cards are a tool; If you link it via the CDN to your web page, you can add custom and responsive cards like the ones below with just one line of HTML.
+Github Repo Cards, the Web Component to show your Github repository cards.
 
 ![Cards](cards-screenshot-1.png)
 
 ## CDN
-First, link the CDN by pasting the following two lines at the head tag of your page.
-
+First of all, import the web component pasting the following script tag at the head tag of your page.  
 ````html
 <script src="https://cdn.jsdelivr.net/npm/github-repo-cards@1.0.10/app.js" defer></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-repo-cards@1.0.10/style.css">
 ````
 
 ## Usage
-
-You can use it, by adding a section tag like the one below at the body tag of your page, and changeing the value of data-user by your github username.
-
+The way of using it is via the custom element github-repo-cards as in the following example:
 ````html
-<section class="github-repo-cards" data-user="github"></section>
+<github-repo-cards data-user="fedeperin"></github-repo-cards>
 ````
 
 ## Parameters
-
 The cards are customizable. You can pass parameters to the section to make changes to the cards.
 
 ### data-user
-
 Your GitHub username.
 
 ### data-target-blank
-
-You can pass the parameter `data-target-blank` to configure if the links are opened by default in a new tab or not. If you want them to open in a new tab, the value wold be `true`, if not, the value wold be `false`.
+The parameter `data-target-blank` is to configure if the links are opened by default in a new tab or not. If you want them to open in a new tab, the value should be `true`, else, the value should be `false`.
 
 ### data-background
-
-You can pass the parameter `data-background` to configure the background of the container. The value can be a color in rgb, rgba, hex, hsl, or the name of the color.
+The parameter `data-background` is to configure the background of the container. The value can be a color in rgb, rgba, hex, hsl, or the name of the color (the supported values of the native CSS background).
--- app.js
+++ app.js
@@ -1,77 +1,101 @@
-const githubRepoCards = document.querySelectorAll('section.github-repo-cards')
+const star = `<svg viewBox="0 0 16 16">
+    <path d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
+</svg>`
+const fork = `<svg viewBox="0 0 16 16">
+    <path d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+</svg>`
+const version = '2.0.0'
 
-let showStars = ''
-let showForks = ''
-let background = '#0d1117'
-let targetBlank = true
-let popular = []
-let unpopular = []
-let star = `<svg viewBox="0 0 16 16"><path d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>`
+class GitHubRepoCards extends HTMLElement {
+    constructor() {
+        super()
+    }
 
-let fork = `<svg viewBox="0 0 16 16"><path d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>`
+    async connectedCallback() {
+        const background = this.getAttribute('data-background')
+        const user = this.getAttribute('data-user')
 
-const newCard = (title, htmlUrl, desc, githubRepoCard, language, stars, forks) => {
-    targetBlank = githubRepoCard.dataset.targetBlank ? "target='_blank' rel='noopener'" : ""
+        await this.addStyling()
+        await this.renderCards(user)
 
-    desc = desc || ''
+        this.style.background = background || '#0d1117'
+    }
 
-    language = language ? `<p class="lang"><span class="lang-color ${ language }"></span> ${ language }</p>` : ''
+    async renderCards(user) {
+        let popular = []
+        let unpopular = []
 
-    showStars = stars <= 0 ? '' : `<a class="stars stars-forks" href="${ htmlUrl + '/stargazers' }" ${ targetBlank }>${ star } <span>${ stars }</span></a>`
+        let res = await fetch(`https://api.github.com/users/${ user || 'github' }/repos`)
+        let repos = await res.json()
 
-    showForks = forks <= 0 ? '' : `<a class="forks stars-forks" href="${ htmlUrl + '/network/members' }" ${ targetBlank }>${ fork } <span>${ forks }</span></a>`
-    
-    return `
-    <div class="github-repo-card">
-        <div>
-            <a href="${ htmlUrl }" ${ targetBlank }>${ title }</a>
-            <p class="desc">${ desc }</p>
-        </div>
-
-        <div class="forks-stars-lang">
-            ${ language }
-             
-            ${showStars}
-            ${showForks}
-        </div>
-    </div>
-    `
-}
-
-const fetchCards = (user, githubRepoCard) => {
-    fetch(`https://api.github.com/users/${ user }/repos`)
-    .then(res => res.json())
-    .then(repos => {
-            repos.forEach(repo => {
-                if(repo.stargazers_count > 0 || repo.forks > 0) {
-                    popular.push(repo)
-                }else {
-                    unpopular.push(repo)
-                }
-            })
-
-            popular = popular.sort((a, b) => b.stargazers_count - a.stargazers_count)
-    
-            if(popular.length < 6) {
-                for(let i = 0; i <= 6 - popular.length; i++) {
-                    popular.push(unpopular[i])
-                }
-            } else {
-                for(let i = 6; i <= popular.length; i++) {
-                    popular.splice(i)
-                }
-            }
-
-            popular = popular.sort((a, b) => b.stargazers_count - a.stargazers_count)
-    
-            popular.forEach(pop => {
-                githubRepoCard.innerHTML += newCard(pop.full_name, pop.html_url, pop.description, githubRepoCard, pop.language, pop.stargazers_count, pop.forks)
-            })
+        repos.forEach(repo => {
+            if (repo.stargazers_count > 0 || repo.forks > 0) return popular.push(repo)
+            unpopular.push(repo)
         })
+
+        popular = popular.sort((a, b) => b.stargazers_count - a.stargazers_count)
+
+        if (popular.length < 6) {
+            for (let i = 0; i <= 6 - popular.length; i++) popular.push(unpopular[i])
+        } else {
+            for (let i = 6; i <= popular.length; i++) popular.splice(i)
+        }
+
+        popular = popular.sort((a, b) => b.stargazers_count - a.stargazers_count)
+
+        popular.forEach(pop => {
+            this.innerHTML += this.newCard(pop)
+        })
+    }
+
+    newCard(cardData) {
+        if(!cardData) return ''
+        const {
+            full_name: title,
+            html_url: htmlUrl,
+            description,
+            language,
+            stargazers_count: stars,
+            forks
+        } = cardData
+        const targetBlank = this.getAttribute('data-target-blank') == 'true' ? `target="_blank" rel="noopener"` : ''
+
+        return `
+        <div class="github-repo-card">
+            <div>
+                <a href="${ htmlUrl }" ${ targetBlank }>${ title }</a>
+                <p class="desc">${ description ?? '' }</p>
+            </div>
+
+            <div class="forks-stars-lang">
+                ${
+                    language ?
+                    `<p class="lang">
+                        <span class="lang-color ${ language }"></span>${ language }
+                    </p>` : ''
+                }
+                
+                ${ 
+                    stars ?
+                    `<a class="stars stars-forks" href="${ htmlUrl + '/stargazers' }" ${ targetBlank }>
+                        ${ star }<span>${ stars }</span>
+                    </a>` : ''
+                }
+                ${
+                    forks ? 
+                    `<a class="forks stars-forks" href="${ htmlUrl + '/network/members' }" ${ targetBlank }>
+                        ${ fork }<span>${ forks }</span>
+                    </a>` : ''
+                }
+            </div>
+        </div>
+        `
+    }
+    async addStyling() {
+        const cssRes = await fetch('./style.css' || `https://cdn.jsdelivr.net/npm/github-repo-cards@${ version }/style.css`)
+        const css = await cssRes.text()
+        this.innerHTML = `<style>${ css }</style>`
+    }
 }
 
-githubRepoCards.forEach(githubRepoCard => {
-    fetchCards(githubRepoCard.dataset.user, githubRepoCard)
-
-    githubRepoCard.style.background = githubRepoCard.dataset.background ? githubRepoCard.dataset.background : '#0d1117'
-})
+customElements.define('github-repo-cards', GitHubRepoCards)
